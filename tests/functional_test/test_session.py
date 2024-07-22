339623619d9b401dd8412a6e245f465a41a05087
wchung <wai.kin.chung@ni.com>
2024-07-22 14:50:27 +0800

    Signed-off-by: wchung <wai.kin.chung@ni.com> Add test open close session with different input

--- tests/functional_test/test_session.py
+++ tests/functional_test/test_session.py
@@ -1,14 +1,26 @@
 import pytest
 
 from nisync import Session
+from nisync._errorcodes import Status
+from nisync.errors import DriverError
 
 
 @pytest.mark.parametrize("reset_device", [True, False])
-def test_session(resource_name, reset_device):
-    with Session(resource_name, reset_device) as session:
-        session.__enter__()
+def test_open_close_session_with_reset(resource_name, reset_device):
+    session = Session(resource_name, reset_device)
+    session.close()
 
 
-def test_close_session(resource_name):
-    with Session(resource_name, False) as session:
+@pytest.mark.parametrize(
+    "resource_name, error_type, error_code", [("FakeDevice", DriverError, Status.DEVICE_NOT_FOUND)]
+)
+def test_open_session_invalid_resource_name(resource_name, error_type, error_code):
+    with pytest.raises(error_type) as expected_info:
+        session = Session(resource_name, True)
         session.close()
+    assert expected_info.value.code == error_code
+
+
+def test_open_close_session_with_input_resource_name(resource_name):
+    session = Session(resource_name, True)
+    session.close()
